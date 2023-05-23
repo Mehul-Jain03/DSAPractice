85b96816e7e5465d842ed2704cd0fea30629d176
Mehul Jain <mehuljain@Mehuls-MacBook-Air.local>
2023-05-23 16:18:05 +0530

    adding array questions

---
+++ DSAPractice/src/main/java/Arrays/BubbleSort.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class BubbleSort {
+
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        bubbleSort(new int[] {3,6,2,9,2,8,20,23,13});
+    }
+}
